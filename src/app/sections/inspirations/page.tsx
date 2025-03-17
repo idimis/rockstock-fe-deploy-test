b72bfe8b57f3ea41791dbcd9e99e6ae8efd416b9
idimis <immanuel.adimas@gmail.com>
2025-03-17 21:54:05 +0700

    add inspirations page

--- src/app/sections/inspirations/page.tsx
+++ src/app/sections/inspirations/page.tsx
@@ -4,6 +4,13 @@ import Navbar from '@/components/common/Navbar';
 import Header from '@/components/common/Header';
 import Image from "next/image";
 
+import inspiration1 from "@/public/inspiration1.jpeg";
+import inspiration2 from "@/public/inspiration2.jpeg";
+import inspiration3 from "@/public/inspiration3.jpg";
+import inspiration4 from "@/public/inspiration4.jpg";
+import inspiration5 from "@/public/inspiration5.jpg";
+import inspiration6 from "@/public/inspiration6.jpg";
+
 const Inspirations: React.FC = () => {
   return (
     <div>
@@ -18,14 +25,7 @@ const Inspirations: React.FC = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Inspiration Item 1 */}
           <div className="bg-white shadow rounded p-4">
-            <div className="w-full h-64 bg-gray-300 mb-4 rounded-lg">
-              {/* Dummy Image Placeholder */}
-              <Image
-                src="https://images.squarespace-cdn.com/content/v1/63dde481bbabc6724d988548/6f6dd238-0ff1-46c9-8ed4-1dccc4fec021/_1d9853d8-fade-4ecb-8f35-5de566acf3dc.jpeg"
-                alt="Dark Academia Library"
-                className="w-full h-full object-cover rounded-lg"
-              />
-            </div>
+            <Image src={inspiration1} alt="Dark Academia Library" className="w-full h-64 object-cover rounded-lg mb-4" />
             <h2 className="text-lg font-semibold mb-2">Dark Academia Library</h2>
             <p className="text-gray-600">
               Create your dream reading nook with shelves and seating that embody classic elegance and intellectual charm.
@@ -34,14 +34,7 @@ const Inspirations: React.FC = () => {
 
           {/* Inspiration Item 2 */}
           <div className="bg-white shadow rounded p-4">
-            <div className="w-full h-64 bg-gray-300 mb-4 rounded-lg">
-              {/* Dummy Image Placeholder */}
-              <Image
-                src="https://images.squarespace-cdn.com/content/v1/63dde481bbabc6724d988548/1f58fb62-3643-4607-b606-d80b8ac3b68f/_0d3afc94-edaf-4809-b7e2-f22c48f98476.jpeg"
-                alt="Gothic Living Room"
-                className="w-full h-full object-cover rounded-lg"
-              />
-            </div>
+            <Image src={inspiration2} alt="Gothic Living Room" className="w-full h-64 object-cover rounded-lg mb-4" />
             <h2 className="text-lg font-semibold mb-2">Gothic Living Room</h2>
             <p className="text-gray-600">
               Elevate your space with ornate furniture and dark tones for a mysterious and dramatic atmosphere.
@@ -50,14 +43,7 @@ const Inspirations: React.FC = () => {
 
           {/* Inspiration Item 3 */}
           <div className="bg-white shadow rounded p-4">
-            <div className="w-full h-64 bg-gray-300 mb-4 rounded-lg">
-              {/* Dummy Image Placeholder */}
-              <Image
-                src="https://images.squarespace-cdn.com/content/v1/59ee0010f43b55d3703661eb/1627386648118-8NFSG636W29RTGG06ZOJ/Emo+statement+wall"
-                alt="Emo-Inspired Workspace"
-                className="w-full h-full object-cover rounded-lg"
-              />
-            </div>
+            <Image src={inspiration3} alt="Emo-Inspired Workspace" className="w-full h-64 object-cover rounded-lg mb-4" />
             <h2 className="text-lg font-semibold mb-2">Emo-Inspired Workspace</h2>
             <p className="text-gray-600">
               Design a creative and moody workspace with bold contrasts and personalized touches.
@@ -65,48 +51,26 @@ const Inspirations: React.FC = () => {
           </div>
 
           {/* Inspiration Item 4 */}
-<div className="bg-white shadow rounded p-4">
-  <div className="w-full h-64 bg-gray-300 mb-4 rounded-lg">
-    {/* Dummy Image Placeholder */}
-    <Image
-      src="https://civilisable.com/wp-content/uploads/2024/01/Italian-Renaissance-Homes-27.1.2024.jpg"
-      alt="Renaissance Revival Room"
-      className="w-full h-full object-cover rounded-lg"
-    />
-  </div>
-  <h2 className="text-lg font-semibold mb-2">Renaissance Revival Room</h2>
-  <p className="text-gray-600">
-    Create a grand space with ornate furniture, classical artwork, and rich textures that reflect the opulence and historical grandeur of the Renaissance.
-  </p>
-</div>
+          <div className="bg-white shadow rounded p-4">
+            <Image src={inspiration4} alt="Renaissance Revival Room" className="w-full h-64 object-cover rounded-lg mb-4" />
+            <h2 className="text-lg font-semibold mb-2">Renaissance Revival Room</h2>
+            <p className="text-gray-600">
+              Create a grand space with ornate furniture, classical artwork, and rich textures that reflect the opulence and historical grandeur of the Renaissance.
+            </p>
+          </div>
 
-{/* Inspiration Item 5 */}
-<div className="bg-white shadow rounded p-4">
-  <div className="w-full h-64 bg-gray-300 mb-4 rounded-lg">
-    {/* Dummy Image Placeholder */}
-    <Image
-      src="https://i.pinimg.com/736x/13/13/85/131385c5e42ca28b266aea420abb5654.jpg"
-      alt="Punk Rock Room"
-      className="w-full h-full object-cover rounded-lg"
-    />
-  </div>
-  <h2 className="text-lg font-semibold mb-2">Punk Rock Room</h2>
-  <p className="text-gray-600">
-    Design an edgy, rebellious space with bold contrasts, eclectic decor, and a mix of vintage punk and modern elements that scream individuality and defiance.
-  </p>
-</div>
-
+          {/* Inspiration Item 5 */}
+          <div className="bg-white shadow rounded p-4">
+            <Image src={inspiration5} alt="Punk Rock Room" className="w-full h-64 object-cover rounded-lg mb-4" />
+            <h2 className="text-lg font-semibold mb-2">Punk Rock Room</h2>
+            <p className="text-gray-600">
+              Design an edgy, rebellious space with bold contrasts, eclectic decor, and a mix of vintage punk and modern elements that scream individuality and defiance.
+            </p>
+          </div>
 
           {/* Inspiration Item 6 */}
           <div className="bg-white shadow rounded p-4">
-            <div className="w-full h-64 bg-gray-300 mb-4 rounded-lg">
-              {/* Dummy Image Placeholder */}
-              <Image
-                src="https://www.bhg.com/thmb/b6hXXmRG9m_GDznOxr1QwkyhSs4=/1244x0/filters:no_upscale():strip_icc()/modern-style-living-room-red-accents-acb3c198-822cc76a4cb64185b2e890a6ebffc921.jpg"
-                alt="Alternative Accent Pieces"
-                className="w-full h-full object-cover rounded-lg"
-              />
-            </div>
+            <Image src={inspiration6} alt="Alternative Accent Pieces" className="w-full h-64 object-cover rounded-lg mb-4" />
             <h2 className="text-lg font-semibold mb-2">Alternative Accent Pieces</h2>
             <p className="text-gray-600">
               Add character to any room with unique decor items that speak to your alternative style.
@@ -120,3 +84,5 @@ const Inspirations: React.FC = () => {
 };
 
 export default Inspirations;
+
+
